b9c63121fed088ad5f49762e26db66bdf8bce6aa
fernando <fernandomarquescontato@gmail.com>
2024-02-16 20:14:58 -0300

    download_combustivel

--- API/main.py
+++ API/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from google.cloud import storage
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
 
@@ -19,3 +20,13 @@ def get_dados(remote_url):
     response = requests.get(remote_url)
     
     return response
+
+app.post("/download_combustivel")
+async def download_combustivel(params: Params):
+    try:
+
+        data = get_dados(params.url)
+        return {"Status": "OK", "Bucket_name": params.bucket_name, "url": params.url}
+
+    except Exception as ex:
+        raise HTTPException(status_code=ex.code, detail=f"{ex}")
